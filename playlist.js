d9cb0ce3a2fb4b49cbcb4fbf4107b5ede0209d36
feven16 <42052151+feven16@users.noreply.github.com>
2018-08-23 09:08:34 -0700

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,95 @@
+var playlist = [
+"https://open.spotify.com/embed/user/azuresav/playlist/3GRLEmEbUc31gB1GjHtcA0", /* 90s POP */
+"https://open.spotify.com/embed/user/azuresav/playlist/3pWco6riu3ZqlNjWrFSrPV",
+"https://open.spotify.com/embed/user/azuresav/playlist/7Id82OihBFr0GxjUbd3hEs",
+"https://open.spotify.com/embed/user/azuresav/playlist/1ktUk1teLhJVaEP4oAXjin",
+"https://open.spotify.com/embed/user/azuresav/playlist/6LlKEUdrNUk0LXVBSWprRU",
+"https://open.spotify.com/embed/user/feven16/playlist/4qG8GHoW84YHZGrSgDPlKO", /* 00s POP */
+"https://open.spotify.com/embed/user/feven16/playlist/59gktS1wZ4OkPscBpJQkSl",
+"https://open.spotify.com/embed/user/feven16/playlist/4vc4Mn7z0lL0a5hDsaFseh",
+"https://open.spotify.com/embed/user/feven16/playlist/6JqtvNx1WmVYmvfmDff1cD",
+"https://open.spotify.com/embed/user/feven16/playlist/4xw1FT35m38bu9337PVhCk",
+"https://open.spotify.com/embed/user/feven16/playlist/5f73bwq12w77yEe6zCu7bp", /* NOW POP */
+"https://open.spotify.com/embed/user/feven16/playlist/4nQuBvT8XYHOfFbtU8kx7a",
+"https://open.spotify.com/embed/user/feven16/playlist/0fteKpIFz8NPkRbwDwGrxY",
+"https://open.spotify.com/embed/user/feven16/playlist/4ppYIrLiCGnkSBtktZkOcK",
+"https://open.spotify.com/embed/user/feven16/playlist/1gSESkdC2m6TrxFN2yPBSP",
+"https://open.spotify.com/embed/user/azuresav/playlist/3nWaRs61Q5PEhyAUTaGRPb", /* 90's COUNTRY */
+"https://open.spotify.com/embed/user/azuresav/playlist/6Efm84jeusgVb6yzjI0x6v",
+"https://open.spotify.com/embed/user/feven16/playlist/7gEAhaWk14OQ1s6n4YCEKN",
+"https://open.spotify.com/embed/user/azuresav/playlist/1gaAtZVLsl8qN6wI3RDYnA",
+"https://open.spotify.com/embed/user/azuresav/playlist/3l1vNgGxlmTN4O25cizYZ5",
+"https://open.spotify.com/embed/user/feven16/playlist/3HZqsOHhghDCAttPyG97jo", /* 00's COUNTRY */
+"https://open.spotify.com/embed/user/feven16/playlist/3BKahCN3XDAb34cSZEyTeD",
+"https://open.spotify.com/embed/user/feven16/playlist/0Uy4uMJH5pNkK3843kil5E",
+"https://open.spotify.com/embed/user/feven16/playlist/2lSY0HXVUt8QC5A4oUHPC6",
+"https://open.spotify.com/embed/user/feven16/playlist/7jCHUWt22lXZOK7tX0qEtT",
+"https://open.spotify.com/embed/user/feven16/playlist/0uX7k3zHycJfz056d6UMhK", /* NOW COUNTRY */
+"https://open.spotify.com/embed/user/feven16/playlist/54i2Ejizan0NZcTKQZZ87X",
+"https://open.spotify.com/embed/user/feven16/playlist/4SPIY6szfKfMIVhgBHaXMM",
+"https://open.spotify.com/embed/user/feven16/playlist/4t4dEYcDZolnSYK2mnr6of",
+"https://open.spotify.com/embed/user/feven16/playlist/6F08XJrNbFp8aq2XrUB71X",
+"https://open.spotify.com/embed/user/azuresav/playlist/49Ir6SYk6m2HequbD5O8g6", /* 90's ROCK */
+"https://open.spotify.com/embed/user/azuresav/playlist/5wBJUXlM3GuwGTuxq6LYQR",
+"https://open.spotify.com/embed/user/azuresav/playlist/0BG9nbdITpYbIHGsuxuUFt",
+"https://open.spotify.com/embed/user/azuresav/playlist/4PHqDL8q4inTZjVZVyZ6yc",
+"https://open.spotify.com/embed/user/azuresav/playlist/16xBBy5r3goctHkYC7MrU2",
+"https://open.spotify.com/embed/user/azuresav/playlist/5VHwQz084NuGWPebAGkm2z", /* 00's ROCK */
+"https://open.spotify.com/embed/user/azuresav/playlist/7IjlH7koXOdUQN9VadD7XH",
+"https://open.spotify.com/embed/user/azuresav/playlist/2XHrgBQNYbB9ovSEOl0CMf",
+"https://open.spotify.com/embed/user/azuresav/playlist/2R9h5xYYMZVwGF4kRG3A4g",
+"https://open.spotify.com/embed/user/azuresav/playlist/69RsZ7ZhzePnfzSfsTQSdV",
+"https://open.spotify.com/embed/user/azuresav/playlist/3LCS6NA6Bqdei1mitR7KOi", /* NOW ROCK */
+"https://open.spotify.com/embed/user/azuresav/playlist/7zNwG1izjtCuiboodr1dri",
+"https://open.spotify.com/embed/user/azuresav/playlist/6YpgFUy2hjGWEGPYn0k9Ko",
+"https://open.spotify.com/embed/user/azuresav/playlist/2qMR2sZLXFFM2QxVUTE3wZ",
+"https://open.spotify.com/embed/user/azuresav/playlist/4S6K08cLLk4968ljmzxOOq",
+"https://open.spotify.com/embed/user/azuresav/playlist/0yQE8HuBUcdIcCz74IZ9cR", /* 90'S RAP */
+"https://open.spotify.com/embed/user/azuresav/playlist/6uFAlgJmI6WtgDwO0VBUrT",
+"https://open.spotify.com/embed/user/azuresav/playlist/1tXhZja52hDP2IAxejzzWI",
+"https://open.spotify.com/embed/user/azuresav/playlist/7s9BZd7W55751fV1OcCSe1",
+"https://open.spotify.com/embed/user/azuresav/playlist/6313NvrcMkrJdEb5qiGUnd",
+"https://open.spotify.com/embed/user/azuresav/playlist/2swKOQmyi0QaPtDexYScXv", /* 00's RAP */
+"https://open.spotify.com/embed/user/azuresav/playlist/423DjXuA1NpyyWSbK52R1P",
+"https://open.spotify.com/embed/user/azuresav/playlist/2IVGAXdgRLxYyXgYS1xMDv",
+"https://open.spotify.com/embed/user/azuresav/playlist/6kPleWvNvqPfDOMK3tIBzw",
+"https://open.spotify.com/embed/user/azuresav/playlist/6sD8JWmZfZcSf2F0wsGArJ",
+"https://open.spotify.com/embed/user/azuresav/playlist/3BNcMQ0hwg0uKZ0ao6JPNk",
+"https://open.spotify.com/embed/user/azuresav/playlist/67aa9URFUFMS2Fz9HY14Lp",
+"https://open.spotify.com/embed/user/azuresav/playlist/2tKjjKoq0AY1uCIjMPwzO3",
+"https://open.spotify.com/embed/user/azuresav/playlist/21rA9eM2QOQf4qrm1qwGn5",
+"https://open.spotify.com/embed/user/azuresav/playlist/3IpuhSYga6oPZaFK5tCZXl"
+];
+
+/* DICT OF VARIABLES AND THEIR INDECES */
+var dictOfVars = {
+    "pop": 0,
+    "country": 15,
+    "rock": 30,
+    "rap": 45,
+
+    "90s": 0,
+    "00s": 5,
+    "now": 10,
+
+    "happy": 0,
+    "love": 1,
+    "chill": 2,
+    "sad": 3,
+    "energy": 4,
+};
+
+var myplaylistIndex = dictOfVars[localStorage.getItem("genre")] + dictOfVars[localStorage.getItem("decade")] + dictOfVars[localStorage.getItem("mood")];
+document.getElementById("playlist").src = playlist[myplaylistIndex];
+
+
+
+
+
+/* IF ALERT IS BETWEEN VAR & DOC.getElementById, THEN Ro90C POPS UP (THE LINK HARDCODED IN PLAYLISTPAGE.HTML)
+   BUT
+   IF ALERT IS ANYWHERE ELSE/NON-EXISTENT, THEN P90H SHOWS UP
+   WHY???????? */
+
+
+/* HOW TO GO DIRECTLY TO PLAYLIST W/O ?? */
